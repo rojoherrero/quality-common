dcdecb42f35961d19a4d15a448c67400351e0d73
rojoherrero <sergio@rojoherrero.es>
2018-11-29 21:42:12 +0100

    logger tweaking

--- logger.go
+++ logger.go
@@ -7,34 +7,39 @@ import (
 	"time"
 )
 
-type Logger interface {
-	Error(msg string)
-	Info(msg string)
-}
+type (
+	Logger interface {
+		Error(msg string)
+		Info(msg string)
+	}
 
-type logger struct {
-	info *log.Logger
-	error *log.Logger
-}
+	logger struct {
+		info  *log.Logger
+		error *log.Logger
+	}
+)
 
 func InitLogger(path string) Logger {
+	file := createLogFIle(path)
+	return &logger{
+		log.New(file, "INFO", log.Ldate|log.Ltime|log.Lshortfile),
+		log.New(file, "ERROR", log.Ldate|log.Ltime|log.Lshortfile)}
+}
+
+func createLogFIle(path string) *os.File {
 	fileName := filepath.Join(path, "/quality-", time.Now().Format("20060102150405"), ".log")
 	file, e := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer file.Close()
 	if e != nil {
 		panic(e)
 	}
-
-	error := log.New(file, "ERROR", log.Ldate|log.Ltime|log.Lshortfile)
-	info := log.New(file, "INFO", log.Ldate|log.Ltime|log.Lshortfile)
-
-	return &logger{info, error}
+	return file
 }
 
-func (l *logger) Error(msg string){
+func (l *logger) Error(msg string) {
 	l.error.Fatalln(msg)
 }
 
-func (l *logger) Info(msg string){
+func (l *logger) Info(msg string) {
 	l.info.Println(msg)
 }

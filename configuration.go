6142a41c372669a584468d22ad108ef0b60a4e81
rojoherrero <sergio@rojoherrero.es>
2018-12-15 20:46:41 +0100

    Just returning dsn data

--- configuration.go
+++ configuration.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 
 	consul "github.com/hashicorp/consul/api"
-	"github.com/jackc/pgx"
-	"github.com/nats-io/go-nats"
+)
+
+const (
+	emptyString = ""
 )
 
 type (
 	Configuration interface {
-		GetDataSources() (*pgx.ConnPool, *nats.Conn)
-		GetServerConfig() (string, string)
+		GetNatsDSN() (string, error)
+		GetPostgresDSN() (string, error)
+		GetServerConfig() (string, string, error)
 	}
 
 	configuration struct {
@@ -21,72 +24,62 @@ type (
 )
 
 // InitConfigService create a new config holding object
-func InitConfigService(prefix string) Configuration {
+func InitConfigService(prefix string) (Configuration, error) {
 	cc, e := consul.NewClient(consul.DefaultConfig())
-	panicIfNil(e)
-	return &configuration{cc: cc, prefix: prefix}
+	if e != nil {
+		return nil, e
+	}
+	return &configuration{cc: cc, prefix: prefix}, nil
 }
 
-func (cfg *configuration) GetServerConfig() (string, string) {
+func (cfg *configuration) GetServerConfig() (string, string, error) {
 	host, _, e := cfg.cc.KV().Get(cfg.prefix+"/server/host", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, emptyString, e
+	}
 	port, _, e := cfg.cc.KV().Get(cfg.prefix+"/server/port", nil)
-	panicIfNil(e)
-	return string(host.Value), string(port.Value)
+	if e != nil {
+		return emptyString, emptyString, e
+	}
+	return string(host.Value), string(port.Value), nil
 }
 
-// GetDataSources returns the postgres and nats connections
-func (cfg *configuration) GetDataSources() (*pgx.ConnPool, *nats.Conn) {
-	db := cfg.connectToPostgres()
-	nc := cfg.connectToNats()
-	return db, nc
-}
-
-func (cfg *configuration) connectToPostgres() *pgx.ConnPool {
-	connCfg := cfg.getPostgresConnectionData()
-	dbPool, e := pgx.NewConnPool(pgx.ConnPoolConfig{ConnConfig: connCfg})
-	panicIfNil(e)
-	return dbPool
-}
-
-func (cfg *configuration) connectToNats() *nats.Conn {
-	// nats://localhost:4222
-	url := cfg.getNATSConnectionData()
-	nc, e := nats.Connect(url)
-	panicIfNil(e)
-	return nc
-}
-
-func (cfg *configuration) getPostgresConnectionData() pgx.ConnConfig {
+func (cfg *configuration) GetPostgresDSN() (string, error) {
 	host, _, e := cfg.cc.KV().Get(cfg.prefix+"/pg/host", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	port, _, e := cfg.cc.KV().Get(cfg.prefix+"/pg/port", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	user, _, e := cfg.cc.KV().Get(cfg.prefix+"/pg/user", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	pwd, _, e := cfg.cc.KV().Get(cfg.prefix+"/pg/pwd", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	dBName, _, e := cfg.cc.KV().Get(cfg.prefix+"/pg/dbName", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		string(host.Value), string(port.Value), string(user.Value), string(pwd.Value), string(dBName.Value))
 
-	cconnCfg, e := pgx.ParseDSN(dsn)
-	panicIfNil(e)
-
-	return cconnCfg
+	return dsn, nil
 }
 
-func (cfg *configuration) getNATSConnectionData() string {
+func (cfg *configuration) GetNatsDSN() (string, error) {
 	host, _, e := cfg.cc.KV().Get("nats/host", nil)
-	panicIfNil(e)
+	if e != nil {
+		return emptyString, e
+	}
 	port, _, e := cfg.cc.KV().Get("nats/port", nil)
-	panicIfNil(e)
-	return fmt.Sprintf("nats://%s:%s", string(host.Value), string(port.Value))
+	if e != nil {
+		return emptyString, e
+	}
+	return fmt.Sprintf("nats://%s:%s", string(host.Value), string(port.Value)), nil
 }
 
-func panicIfNil(e error) {
-	if e != nil {
-		panic(e)
-	}
-}

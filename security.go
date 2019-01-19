4b8e1177c5fb678b89206801f49f6d455335c258
rojoherrero <sergio@rojoherrero.es>
2019-01-19 19:38:07 +0100

    Deleting old code and adding common structs for login

---
+++ security.go
@@ -0,0 +1,43 @@
+package common
+
+import "encoding/json"
+
+// LogInRequest holds user credentials
+type LogInRequest struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+// UnmarshalLogInRequest Unmarshal an array of bytes to the struct
+func UnmarshalLogInRequest(data []byte) (LogInRequest, error) {
+	var r LogInRequest
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+// Marshal the struct to an byte array
+func (r *LogInRequest) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+// LogInResponse holds the resulto of the authetication
+type LogInResponse struct {
+	UserMatch     bool     `json:"userMatch,omitempty"`
+	PasswordMatch bool     `json:"passwordMatch,omitempty"`
+	Username      string   `json:"username,omitempty"`
+	Fullname      string   `json:"fullname,omitempty"`
+	Department    string   `json:"department,omitempty"`
+	Roles         []string `json:"roles,omitempty"`
+}
+
+// UnmarshalLogInResponse Unmarshal an array of bytes to the struct
+func UnmarshalLogInResponse(data []byte) (LogInResponse, error) {
+	var r LogInResponse
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+// Marshal the struct to an byte array
+func (r *LogInResponse) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
